d83aa42099b9a69d3f86f7052543170f77a9958d
Filipe <lipimiranda1999@gmail.com>
2024-09-15 21:41:47 -0300

    Fazendo a entrada de dados via terminal e criando um vetor

---
+++ monitoramento-vendas.R
@@ -0,0 +1,5 @@
+# Capturando a entrada de dados com as vendas diárias dos vendedores
+#
+# Faça a entrada de dados no terminal.
+# Ela será encerrada no decimo elemento do vetor
+vendas_diarias_vendedores <- scan(n = 10)
