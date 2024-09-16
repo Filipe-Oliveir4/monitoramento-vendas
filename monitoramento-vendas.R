e2e69e087f71cb94b56a363f4bcf24d4bc9bc150
Filipe <lipimiranda1999@gmail.com>
2024-09-15 21:46:53 -0300

    Criação de um vetor lógico de vendedores que atingiram a meta

--- monitoramento-vendas.R
+++ monitoramento-vendas.R
@@ -3,3 +3,6 @@
 # Faça a entrada de dados no terminal.
 # Ela será encerrada no decimo elemento do vetor
 vendas_diarias_vendedores <- scan(n = 10)
+
+#Verificando os vendedores que atigiram a meta mínima de 20 vendas
+vendedores_que_atingiram_meta <- vendas_diarias_vendedores >= 20
