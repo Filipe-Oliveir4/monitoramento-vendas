c3b89c3453b5259b1c724901d909503e47da4c1c
Filipe <lipimiranda1999@gmail.com>
2024-09-15 22:15:40 -0300

    Fazendo a impressão do relatório usando a função length, cat,a estrutura de repetiçaõ for e estrutura de decisão if | else

--- monitoramento-vendas.R
+++ monitoramento-vendas.R
@@ -12,3 +12,12 @@ qtd_vendedores_que_atingiram_meta <- sum(vendedores_que_atingiram_meta)
 
 #Substituir qualquer valor de vendas inferior a 15 por 15
 vendas_diarias_vendedores[vendas_diarias_vendedores < 15] <- 15
+
+#Gerando um “relatório” de desempenho
+for (i in 1:length(vendedores_que_atingiram_meta)) {
+  if (vendedores_que_atingiram_meta[i]) {
+    cat("Vendedor", i, "atingiu a meta.\n")
+  } else {
+    cat("Vendedor", i, "não atingiu a meta.\n")
+  }
+}
